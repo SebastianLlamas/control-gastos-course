94b4b8489309fc7466994e4a7c49e837057852b7
SEBASTIAN ISAI LLAMAS ZAMUDIO <sllamas@octanesystems.mx>
2023-08-06 03:51:55 -0700

    New project, added html and css

---
+++ src/components/NuevoPresupuesto.jsx
@@ -0,0 +1,21 @@
+
+
+const NuevoPresupuesto = () =>{
+    return(
+        <div className="contenedor-presupuesto contenedor sombra">
+            <form className="formulario">
+                <div className="campo"> 
+                    <label >Definir Presupuesto</label>
+                    <input 
+                        type="text"
+                        className="nuevo-presupuesto" 
+                        placeholder="Añade tu Presupuesto"
+                    />
+                </div>
+                <input type="submit" value="Añadir"></input>
+            </form>
+        </div>
+    );
+}
+
+export default NuevoPresupuesto;
